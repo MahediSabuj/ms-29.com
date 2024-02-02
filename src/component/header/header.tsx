b66a6696d6660dc6800786bbe30f3c0a3f0eee92
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2024-02-02 20:05:54 +0600

    professional profile link added

--- src/component/header/header.tsx
+++ src/component/header/header.tsx
@@ -1,9 +1,13 @@
 import Navigation from "@/component/navigation/navigation";
+import IconBar from "@/component/icon-bar/icon-bar";
+
+import styles from "./header.module.css";
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <Navigation/>
+      <IconBar/>
     </header>
   );
 }
