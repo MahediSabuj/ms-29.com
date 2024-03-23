4a23acff0b6fba2ef1ec92719673ea4629fc1923
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2024-03-24 00:30:40 +0600

    ssl certificate: separate as section

--- src/app/aws/ec2/install-ssl-certificate-from-pfx-file/page.tsx
+++ src/app/aws/ec2/install-ssl-certificate-from-pfx-file/page.tsx
@@ -46,31 +46,40 @@ export default function InstallCertificate() {
           browser and the site to protect sensitive information. Let&apos;s explore the process of installing SSL certificate
           from PFX file, providing step-by-step instructions for a seamless setup.
         </div>
-        <div className="pt-3 py-1">
+        <h2 id="export-certificate" className="text-xl mt-6">
+          <strong>Extract Private Key and Certificate from PFX file</strong>
+        </h2>
+        <div className="py-1">
           PFX (Personal Exchange Format) file, also known as a PKCS #12 file, is a binary format used to store a certificate and its corresponding private
           key. OpenSSL command can be used to extract both the certificate and private key from a PFX file.
         </div>
         <Highlight code={extract_certificate} language="shell" path=""/>
-        <div className="pt-1">
+        <div className="py-3">
           In case the PFX file is password protected, you will be prompted to enter the password as &quot;Enter Import Password&quot;.
           If the password is valid, website.com.txt file will be saved in the current directory. The first section of the website.com.txt file
           contains the private key, which can be saved as website.com.key. The second section contains the certificate body,
           which can be saved as website.com.crt file.
         </div>
-        <div className="pt-4">
+        <div>
           Once the key and crt files are prepared, you can proceed to configure them on your http server, such as Apache or Nginx.
-          Alternatively, if you are using AWS Elastic Load Balancer, you can utilize AWS Certificate Manager to manage your
+          Alternatively, if you are using AWS Elastic Load Balancer (ELB), you can utilize AWS Certificate Manager (ACM) to manage your
           SSL certificates and associate them with your load balancer.
         </div>
-        <div className="pt-3">
+        <h2 id="configure-certificate-nginx" className="text-xl mt-6">
+          <strong>Configure SSL Certificate using Nginx</strong>
+        </h2>
+        <div>
           To configure SSL in Nginx, need to upload the required files to the server using FileZilla or another FTP client.
           Once uploaded, reference these files in the Nginx server block configuration. Finally, restart Nginx to apply the changes.
         </div>
         <Highlight code={nginx_conf} language="nginx" path="website.com.conf"/>
-        <div className="pt-6 pb-2">
+        <h2 id="configure-certificate-aws" className="text-xl mt-6">
+          <strong>Configure SSL Certificate using ACM and ELB</strong>
+        </h2>
+        <div className="pb-2">
           To import a certificate using AWS Certificate Manager, navigate to the Certificate Manager service and select
           the &quot;Import Certificate&quot; option from the left-hand menu. Paste the certificate and private key into
-          the respective text boxes, add tags if necessary, and review the details before confirming the import certificate.
+          the respective text boxes, add tags accordingly, and review the details before confirming the import certificate.
         </div>
         <Image src={import_ssl_certificate} className="border"
           alt="Import SSL Certificate in AWS Certificate Manager">
