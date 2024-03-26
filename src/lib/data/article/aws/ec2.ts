3c72974af24cb761c7c24c1c9a7ea621f5b5cc67
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2024-03-26 18:09:50 +0600

    new draft article added

--- src/lib/data/article/aws/ec2.ts
+++ src/lib/data/article/aws/ec2.ts
@@ -13,6 +13,17 @@ export const INSTALL_SSL_CERTIFICATE_FROM_PFX_FILE: IArticleItem = {
   active: true
 }
 
+export const SETUP_CI_CD_PIPELINE_TO_DEPLOY_NODEJS_APP_TO_AWS_EC2: IArticleItem = {
+  title: "Setup CI/CD Pipeline to Deploy Node.js App to AWS EC2",
+  description: ``,
+  url: "/aws/ec2/setup-ci-cd-pipeline-to-deploy-nodejs-app-to-aws-ec2",
+  publishDate: "April 24, 2024",
+  modifiedDate: "April 24, 2024",
+  topics: [ TOPICS.AWS_EC2 ],
+  active: false
+}
+
 export const AWS_EC2: IArticleItem[] = [
-  INSTALL_SSL_CERTIFICATE_FROM_PFX_FILE
+  INSTALL_SSL_CERTIFICATE_FROM_PFX_FILE,
+  SETUP_CI_CD_PIPELINE_TO_DEPLOY_NODEJS_APP_TO_AWS_EC2
 ].filter(m => m.active);
