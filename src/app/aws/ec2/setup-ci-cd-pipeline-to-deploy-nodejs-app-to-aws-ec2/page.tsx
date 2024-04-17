2c1a0e96ccbbcd86dac3dc79c68847c5e1b0334f
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2024-04-18 03:56:48 +0600

    ci/cd pipeline

--- src/app/aws/ec2/setup-ci-cd-pipeline-to-deploy-nodejs-app-to-aws-ec2/page.tsx
+++ src/app/aws/ec2/setup-ci-cd-pipeline-to-deploy-nodejs-app-to-aws-ec2/page.tsx
@@ -1,14 +1,42 @@
+import { Metadata } from "next";
+
 import Article from "@/components/article/article";
+import { IBreadCrumb } from "@/types/breadcrumb";
+import BreadCrumb from "@/components/breadcrumb/breadcrumb";
 import { SETUP_CI_CD_PIPELINE_TO_DEPLOY_NODEJS_APP_TO_AWS_EC2 as ARTICLE } from "@/lib/data/article/aws/ec2";
+
+export const metadata: Metadata = {
+  title: ARTICLE.title,
+  description: ARTICLE.description,
+  alternates: {
+    canonical: ARTICLE.url
+  }
+};
+
+const breadcrumbs : IBreadCrumb = {
+  items: [{
+    title: "AWS EC2",
+    url: "/aws/ec2"
+  }],
+  current: ARTICLE.title
+}
 
 export default function SetupCICDPipeline() {
   return (
     <div>
+      <BreadCrumb {...breadcrumbs}/>
       <article itemScope itemType="https://schema.org/Article">
         <Article
           title={ARTICLE.title}
           publishDate={ARTICLE.publishDate}
           modifiedDate={ARTICLE.modifiedDate}/>
+        <div>
+          <section className="pt-6">
+            In today&apos;s fast-paced software development environment, Continuous Integration and Continuous Deployment (CI/CD) 
+            pipelines have become essential tools. These pipelines streamline the development process, automate testing, and expedite 
+            application deployment, making it faster, more reliable, and less error-prone.
+          </section>
+        </div>  
       </article>
     </div>
   );
--- src/lib/data/article/aws/ec2.ts
+++ src/lib/data/article/aws/ec2.ts
@@ -15,7 +15,9 @@ export const INSTALL_SSL_CERTIFICATE_FROM_PFX_FILE: IArticleItem = {
 
 export const SETUP_CI_CD_PIPELINE_TO_DEPLOY_NODEJS_APP_TO_AWS_EC2: IArticleItem = {
   title: "Setup CI/CD Pipeline to Deploy Node.js App to AWS EC2",
-  description: ``,
+  description: `In today's fast-paced software development environment, Continuous Integration and Continuous Deployment (CI/CD) 
+    pipelines have become essential tools. These pipelines streamline the development process, automate testing, and expedite 
+    application deployment, making it faster, more reliable, and less error-prone.`,
   url: "/aws/ec2/setup-ci-cd-pipeline-to-deploy-nodejs-app-to-aws-ec2",
   publishDate: "May 24, 2024",
   modifiedDate: "May 24, 2024",
