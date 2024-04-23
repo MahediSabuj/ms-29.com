5ca7eec3c8b65dc204e6d486a1cce3c8c7fc5c9f
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2024-04-23 16:54:29 +0600

    powermockito dependency

--- src/app/aem/code-coverage/mock-static-methods-with-mockito/page.tsx
+++ src/app/aem/code-coverage/mock-static-methods-with-mockito/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import Article from "@/components/article/article";
 import { IBreadCrumb } from "@/types/breadcrumb";
 import BreadCrumb from "@/components/breadcrumb/breadcrumb";
+import Highlight from "@/components/highlight/highlight";
 import { MOCK_STATIC_METHODS_MOCKITO as ARTICLE } from "@/lib/data/article/aem/code-coverage";
 
 export const metadata: Metadata = {
@@ -12,6 +13,25 @@ export const metadata: Metadata = {
     canonical: ARTICLE.url
   }
 };
+
+const power_mockito = 
+`<properties>
+  <powermock.version>2.0.2</powermock.version>
+</properties>
+<dependencies>
+  <dependency>
+    <groupId>org.powermock</groupId>
+    <artifactId>powermock-module-junit4</artifactId>
+    <version>\${powermock.version}</version>
+    <scope>test</scope>
+  </dependency>
+  <dependency>
+    <groupId>org.powermock</groupId>
+    <artifactId>powermock-api-mockito2</artifactId>
+    <version>\${powermock.version}</version>
+    <scope>test</scope>
+  </dependency>
+</dependencies>`;
 
 const breadcrumbs : IBreadCrumb = {
   items: [{
@@ -38,8 +58,9 @@ export default function MockStaticMethod() {
           </section>
           <section className="pt-3">
             Over the years, different approaches have been adopted for mocking static methods. Prior to Mockito v3.4.0, PowerMockito was 
-            utilized to mock static methods and it was compatible with JUnit 4.
+            utilized to mock static methods and was compatible with JUnit 4 which you can integrate by adding the following dependency to the pom.xml file.
           </section>
+          <Highlight code={power_mockito} language="xml" path="pom.xml"/>
         </div>
       </article>
     </div>
