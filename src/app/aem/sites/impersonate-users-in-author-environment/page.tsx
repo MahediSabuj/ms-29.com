48c4d60969d4ee89a6657a5e95d41d08e93c43e7
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2025-01-11 22:58:33 +0600

    impersonate user in aem author

--- src/app/aem/sites/impersonate-users-in-author-environment/page.tsx
+++ src/app/aem/sites/impersonate-users-in-author-environment/page.tsx
@@ -1,10 +1,14 @@
 import { Metadata } from "next";
+import Image from "next/image";
 
 import Article from "@/components/article/article";
 import { IBreadCrumb } from "@/types/breadcrumb";
 import BreadCrumb from "@/components/breadcrumb/breadcrumb";
-import { IMPERSONATE_USERS_AUTHOR_ENVIRONMENT as ARTICLE } from "@/lib/data/article/aem/sites";
 import TOPICS from "@/lib/data/article/topics";
+import { IMPERSONATE_USERS_AUTHOR_ENVIRONMENT as ARTICLE } from "@/lib/data/article/aem/sites";
+
+import ADD_USERS_AS_IMPERSONATORS from "./assets/add-users-as-impersonators.png";
+import IMPERSONATE_AS_USER from "./assets/impersonate-as-users.png";
 
 export const metadata: Metadata = {
   title: ARTICLE.title,
@@ -36,7 +40,15 @@ export default function ImpersonateUsers() {
             To troubleshoot issues, we often need to impersonate users in the author environment. With the Impersonate functionality, a user can
             work on behalf of another user.
           </section>
-        </div>  
+          <section className="pt-3">
+            Recently, we encountered an issue where  user was unable to edit pages due to CSS not loading correctly. To investigate the issue, we
+            attempted to impersonate the user to better understand the issue.
+          </section>
+          <Image className="border p-1 mt-2"
+             src={ADD_USERS_AS_IMPERSONATORS} alt="Add Users as Impersonators"/>
+          <Image className="border p-1 mt-2"  height={400}
+             src={IMPERSONATE_AS_USER} alt="Impersonate as Users"/>
+        </div>
       </article>
     </div>
   );
