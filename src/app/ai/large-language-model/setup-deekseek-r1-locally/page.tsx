2433af451ae90473f37b155bd9fa339794a28d8c
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2025-02-03 13:14:58 +0600

    run deepseek-r1 with ollama

--- src/app/ai/large-language-model/setup-deekseek-r1-locally/page.tsx
+++ src/app/ai/large-language-model/setup-deekseek-r1-locally/page.tsx
@@ -1,11 +1,12 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 import Article from "@/components/article/article";
 import { IBreadCrumb } from "@/types/breadcrumb";
 import BreadCrumb from "@/components/breadcrumb/breadcrumb";
+import Highlight from "@/components/highlight/highlight";
 import { SETUP_DEEPSEEK_LOCALLY as ARTICLE } from "@/lib/data/article/ai/large-language-model";
 import TOPICS from "@/lib/data/article/topics";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: ARTICLE.title,
@@ -14,6 +15,9 @@ export const metadata: Metadata = {
     canonical: ARTICLE.url
   }
 };
+
+const PULL_DEEPSEEK_R1 = `ollama pull deepseek-r1:8b`;
+const RUN_DEEPSEEK_R1 = `ollama run deepseek-r1:8b`;
 
 const breadcrumbs : IBreadCrumb = {
   items: [{
@@ -34,12 +38,35 @@ export default function OsgiConfiguration() {
           modifiedDate={ARTICLE.modifiedDate}/>
         <div>
           <section className="pt-6">
-            DeepSeek R1 is a powerful conversational AI model, and integrating it with Chainlit UI allows to create an interactive
-            and user friendly interface. Using Ollama, allows to run LLMs locally, making it simple to deploy and interact with models like DeepSeek R1.
+            DeepSeek R1 is a powerful conversational AI model, and integrating it with Chainlit UI allows to create an
+            interactive
+            and user friendly interface. Using Ollama, allows to run LLMs locally, making it simple to deploy and
+            interact with models like DeepSeek R1.
+          </section>
+          <h2 className="text-xl mt-4">
+            <strong>Install Ollama</strong>
+          </h2>
+          <section>
+            To download Ollama, visit the official <Link href="https://github.com/ollama/ollama" target="_blank" className="text-blue-600">Ollama GitHub Repository</Link> and
+            select the version compatible with your operating system (Windows, macOS, or Linux). Alternatively, you can pull the
+            Docker image if preferred.
+          </section>
+          <h2 className="text-xl mt-4">
+            <strong>Run DeepSeek R1 using Ollama</strong>
+          </h2>
+          <section>
+            Ollama simplifies model management. To download choose the <Link className="text-blue-600" target="_blank" href="https://ollama.com/library/deepseek-r1:8b">appropriate DeepSeek version</Link> based on
+            your local environment. I am able to run the <strong>8b</strong> model on an Apple M1 Pro with 16GB of memory. To Setup DeekSeek R1, run:
+            <Highlight code={PULL_DEEPSEEK_R1} language="shell" path=""/>
+            <div className="pt-1">This command fetches the DeepSeek R1 model and prepares it for local use.</div>
           </section>
           <section className="pt-3">
-            To download Ollama, visit the official <Link href="https://github.com/ollama/ollama" target="_blank" className="text-blue-600">Ollama GitHub Repository</Link> and select the
-            version compatible with your operating system (Windows, macOS, or Linux). Alternatively, you can pull the Docker image if preferred.
+            To start an interactive session with DeepSeek R1 using the following command:
+            <Highlight code={RUN_DEEPSEEK_R1} language="shell" path=""/>
+            <div className="pt-1">
+              This will launch the model, allowing you to interact with it directly in your terminal. Once the model is running,
+              you can start typing prompts and receive responses in real-time.
+            </div>
           </section>
         </div>
       </article>
--- src/components/highlight/highlight.tsx
+++ src/components/highlight/highlight.tsx
@@ -7,10 +7,14 @@ export default function HighlightCode({ code, language, path, image }: Highlight
     language
   }).value;
 
+  const borderClass = path ? "border-t-0" : "";
+
   return (
     <section className="pt-1">
-      <div className="text-white px-2 py-1 bg-aem">{path}</div>
-      <div className="border-neutral-300 border-2 border-solid border-t-0">
+      {path &&
+        <div className="text-white px-2 py-1 bg-aem">{path}</div>
+      }
+      <div className={`border-neutral-300 border-2 border-solid ${borderClass}`}>
         <div className="p-2 bg-neutral-100 overflow-x-auto">
           <code className="whitespace-pre text-[14px]"
             dangerouslySetInnerHTML={{ __html: highlighted_code }}/>
