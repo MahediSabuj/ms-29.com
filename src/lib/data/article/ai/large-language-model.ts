e2dc71863db25bc9f17f37f2cae59bda9a19e2a6
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2025-02-03 17:59:37 +0600

    update deepseek article

--- src/lib/data/article/ai/large-language-model.ts
+++ src/lib/data/article/ai/large-language-model.ts
@@ -5,7 +5,7 @@ export const SETUP_DEEPSEEK_LOCALLY: IArticleItem = {
     title: "Setup and Configure DeepSeek R1 in Local Environment",
     description: `DeepSeek R1 is a powerful conversational AI model, and integrating it with Chainlit UI allows to create an interactive 
     and user friendly interface. Using Ollama, allows to run LLMs locally, making it simple to deploy and interact with models like DeepSeek R1.`,
-    url: `${TOPICS.AI_LLM.url}/setup-deekseek-r1-locally`,
+    url: `${TOPICS.AI_LLM.url}/setup-deekseek-r1-local-environment`,
     publishDate: "February 04, 2025",
     modifiedDate: "February 04, 2025",
     topics: [ TOPICS.AI_LLM ],
