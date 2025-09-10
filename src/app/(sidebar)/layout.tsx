630275c27fca2cb20e1e9c1314e30540320482b8
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2025-09-10 17:32:21 +0600

    remove duplicate ad banner

--- src/app/(sidebar)/layout.tsx
+++ src/app/(sidebar)/layout.tsx
@@ -10,14 +10,6 @@ export default function SidebarLayout({
     <div className="container mx-auto py-8 grow">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 md:flex gap-8">
         <main className="md:w-3/4 w-full">
-          <GooglePublisherTag
-            container="div-gpt-ad-1738911994343-0"
-            adUnit="ms29-banner"
-            sizes={[[728, 90], [320, 50], [970, 90]]}
-            sizeMapping={[
-              [[768, 0], [[728, 90]]],
-              [[0, 0], [[320, 50]]]
-            ]}/>
           <div>{children}</div>
         </main>
         <aside className="md:w-1/4 w-full md:pt-0 pt-8">
